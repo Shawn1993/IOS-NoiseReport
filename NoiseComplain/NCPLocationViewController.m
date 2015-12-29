cf9b338b4af32a7cd401fd999cd560b808ba75ef
wuxiaohui <343270711@qq.com>
2015-12-29 22:42:24 +0800

    修改了一点点

--- NoiseComplain/NCPLocationViewController.m
+++ NoiseComplain/NCPLocationViewController.m
@@ -8,9 +8,11 @@
 
 #import "NCPLocationViewController.h"
 #import <BaiduMapAPI_Map/BMKMapComponent.h>
+#import <BaiduMapAPI_Location/BMKLocationComponent.h>
+
 
 #pragma mark - private interface
-@interface NCPLocationViewController (){
+@interface NCPLocationViewController() <BMKMapViewDelegate,BMKLocationServiceDelegate>{
     
     
 }
@@ -18,6 +20,8 @@
 @property (weak, nonatomic) IBOutlet UIView *mapViewContainer;
 
 @property (strong, nonatomic) BMKMapView *mapView;
+
+@property (strong, nonatomic) BMKLocationService *locationService;
 
 - (IBAction)doneButtonClick:(id)sender;
 
@@ -29,7 +33,22 @@
 
 -(void)viewDidLoad{
     self.mapView = [[BMKMapView alloc] init];
+    self.mapView.showsUserLocation = YES ;
+    self.mapView.userTrackingMode = BMKUserTrackingModeFollow;
     [self.mapViewContainer addSubview:self.mapView];
+    
+    self.locationService = [[BMKLocationService alloc] init];
+    [self.locationService startUserLocationService];
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    self.mapView.delegate = self;
+    self.locationService.delegate = self;
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+    self.mapView.delegate = nil;
+    self.locationService.delegate = nil;
 }
 
 -(void)viewDidLayoutSubviews{
@@ -42,4 +61,45 @@
         
     }];
 }
+
+#pragma  mark - BMKLocationServiceDelegate
+
+/**
+ *在将要启动定位时，会调用此函数
+ */
+- (void)willStartLocatingUser{
+    
+}
+
+/**
+ *在停止定位后，会调用此函数
+ */
+- (void)didStopLocatingUser{
+    
+}
+
+/**
+ *用户方向更新后，会调用此函数
+ *@param userLocation 新的用户位置
+ */
+- (void)didUpdateUserHeading:(BMKUserLocation *)userLocation{
+    
+}
+
+/**
+ *用户位置更新后，会调用此函数
+ *@param userLocation 新的用户位置
+ */
+- (void)didUpdateBMKUserLocation:(BMKUserLocation *)userLocation{
+    [self.mapView updateLocationData:userLocation];
+}
+
+/**
+ *定位失败后，会调用此函数
+ *@param error 错误号
+ */
+- (void)didFailToLocateUserWithError:(NSError *)error{
+    
+}
+
 @end
